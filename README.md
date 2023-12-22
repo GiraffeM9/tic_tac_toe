b4494848905d80232157b0ffd2fa58606ad9be09
GiraffeM9 <76228570+GiraffeM9@users.noreply.github.com>
2023-12-22 22:05:38 +1300

    modified:   README.md 	new file:   ai_player.py 	new file:   game.py 	new file:   notepad.txt 	new file:   player.py 	modified:   tic_tac_toe.py 	new file:   user.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-ECHO is on.
+This is a python program made by Mayita Denenga 
+
+Currently a university student at the University of Waikato (2023)
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,14 @@
-ECHO is on.
+# initialise board
+
+# example of list comprehension
+board = [['_' for _ in range(3)] for _ in range(3)]
+
+
+
+# main function to run the program
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
